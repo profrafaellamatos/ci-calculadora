7f3830ffa129bef1201ff1cabfb69c1b910bf126
Rafaella Matos <prof.rafaella.matos@gmail.com>
2023-09-28 10:12:25 -0300

    forcing an error

--- calculator.test.js
+++ calculator.test.js
@@ -3,5 +3,5 @@ const add = require('./calculator')
 test("Add two numbers", ()=>{
     expect(add(1,2)).toBe(3)
     expect(add(-1,2)).toBe(1)
-    expect(add(0,0)).toBe(0)
+    expect(add(0,0)).toBe(10)
 })
